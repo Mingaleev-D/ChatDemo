5bc77272cca9633ca7b4cd3ba06287e70699b038
Mingaleev-D92 <mingaleev,d92@mail.ru>
2023-01-03 12:28:54 +0300

    экран настроек

--- app/src/main/java/com/example/chatdemo/ui/fragments/SettingFragment.kt
+++ app/src/main/java/com/example/chatdemo/ui/fragments/SettingFragment.kt
@@ -1,5 +1,7 @@
 package com.example.chatdemo.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
 import com.example.chatdemo.R
 import com.example.chatdemo.ui.fragments.base_fragment.BaseFragment
 
@@ -8,6 +10,12 @@ class SettingFragment : BaseFragment(R.layout.fragment_setting) {
 
    override fun onResume() {
       super.onResume()
+      setHasOptionsMenu(true)
+
+   }
+
+   override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+      activity?.menuInflater?.inflate(R.menu.settings_action_menu, menu)
 
    }
 
